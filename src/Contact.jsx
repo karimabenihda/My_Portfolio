ca6494edf4210fa0e0d62dc37fa1dd90898a5928
defaltastra <defaltastra@gmail.com>
2024-11-28 17:16:32 +0100

    some minor changes ;)

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,28 +2,37 @@ import React from 'react';
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
-import './contact.css'; 
+import './contact.css';
 
 function Contact() {
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(() => {
+      alert('Copied to clipboard!');
+    }).catch(err => {
+      alert('Failed to copy text: ', err);
+    });
+  };
+
   return (
     <div className='container'>
       <h1 className='title'>Contact</h1>
       <p className='subtitle'>
-        Feel <span className='highlight'> Free </span>To Contact Me
+        Feel <span className='highlight'>Free</span> to Contact Me
       </p>
       <ol className='list'>
-        <li className='item'>
+        <li className='item' onClick={() => copyToClipboard('+212 675600489')}>
           <FaPhoneAlt className='icon' /> +212 675600489
         </li>
-        <li className='item'>
+        <li className='item' onClick={() => copyToClipboard('karimabenihda@gmail.com')}>
           <MdOutlineEmail className='icon' /> karimabenihda@gmail.com
         </li>
         <li className='item'>
-          <FaGithub className='icon' /> <a href="https://github.com/karimabenihda" className='link' style={{color:'#feb7c3'}}>github.com/karimabenihda</a>
+          <FaGithub className='icon' /> 
+          <a href="https://github.com/karimabenihda" className='link' style={{color:'#feb7c3'}}>github.com/karimabenihda</a>
         </li>
       </ol>
     </div>
-  )
+  );
 }
 
 export default Contact;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,45 +1,48 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import karima from '/karima.png'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import karima from '/karima.png';
 import { IoMenu } from "react-icons/io5";
-import { useState } from 'react';
-import "./index.css"
+import './index.css';
+
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const handleDarkModeChange = () => {
+      const isDark = document.body.classList.contains('darkmode--activated');
+      setIsDarkMode(isDark);
+    };
+
+    handleDarkModeChange();
+    const observer = new MutationObserver(handleDarkModeChange);
+    observer.observe(document.body, { attributes: true, attributeFilter: ['class'] });
+
+    return () => observer.disconnect();
+  }, []);
 
   function toggleMenu() {
     setIsMenuOpen(prevState => !prevState);
   }
+
   return (
-<div className="container" style={{marginBottom:'40px'}}>
-    <nav className="nav justify-content-center  ">
-        <Link className="navbar-brand" to="/" ><img src={karima} alt="" /></Link>
-        
+    <div className="container" style={{ marginBottom: '40px' }}>
+      <nav className={`nav justify-content-center ${isDarkMode ? 'dark-nav' : ''}`}>
+        <Link className="navbar-brand" to="/">
+          <img src={isDarkMode ? '/karima-white.png' : karima} alt="Karima" />
+        </Link>
+
         <div className={`nav-items ${isMenuOpen ? 'active' : ''}`}>
-          <Link className="nav-link active" to="/" style={{color:'#2A3C59' , fontWeight:'bold',fontStyle: 'normal'}} >Home</Link>
-          <Link className="nav-link" to="/about" style={{color:'#2A3C59' , fontWeight:'bold',fontStyle: 'normal'}}>About me</Link>
-          <Link className="nav-link" to="/skills" style={{color:'#2A3C59' , fontWeight:'bold',fontStyle: 'normal'}}>Skills</Link>
-          <Link className="nav-link" to="/projects" style={{color:'#2A3C59' , fontWeight:'bold',fontStyle: 'normal'}}>Projects</Link>
-          <Link to="/contact" className="nav-link"  style={{color:'#2A3C59' , fontWeight:'bold',fontStyle: 'normal'}}>Contact</Link>
+          <Link className="nav-link active" to="/" style={{ color: isDarkMode ? 'white' : '#2A3C59', fontWeight: 'bold', fontStyle: 'normal' }}>Home</Link>
+          <Link className="nav-link" to="/about" style={{ color: isDarkMode ? 'white' : '#2A3C59', fontWeight: 'bold', fontStyle: 'normal' }}>About me</Link>
+          <Link className="nav-link" to="/skills" style={{ color: isDarkMode ? 'white' : '#2A3C59', fontWeight: 'bold', fontStyle: 'normal' }}>Skills</Link>
+          <Link className="nav-link" to="/projects" style={{ color: isDarkMode ? 'white' : '#2A3C59', fontWeight: 'bold', fontStyle: 'normal' }}>Projects</Link>
+          <Link to="/contact" className="nav-link" style={{ color: isDarkMode ? 'white' : '#2A3C59', fontWeight: 'bold', fontStyle: 'normal' }}>Contact</Link>
         </div>
         <IoMenu className="menu-icon" onClick={toggleMenu} />
-
-    </nav>
- 
-
-
-
-
-
-
-
-
-
-
-
-
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
